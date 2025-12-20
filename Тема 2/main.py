d373a0489dfb647b8f00bcc65ec5841731e684bf
Андрей Боев <a.boev.it@gmail.com>
2025-12-20 18:17:29 +0300

    NEW: ЕГЭ Тема 3

--- Тема 2/main.py
+++ Тема 2/main.py
@@ -1,6 +1,6 @@
 from itertools import product
 
-# Способ перебора при помощи product
+# РЎРїРѕСЃРѕР± РїРµСЂРµР±РѕСЂР° РїСЂРё РїРѕРјРѕС‰Рё product
 print('x y z w')
 for x, y, z, w in product([1, 0], repeat=4):
     if not((not(z) == y) <= ((w and not(x)) == (y and x))):

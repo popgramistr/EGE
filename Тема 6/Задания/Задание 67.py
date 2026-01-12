11511ade4fcf3bc21ce1837848a5c00a9c87676a
Андрей Боев <a.boev.it@gmail.com>
2026-01-12 18:25:19 +0300

    NEW: Тема 6

---
+++ Тема 6/Задания/Задание 67.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 67, answer, 'ac627ab1ccbdb62ec96e702f07f6425b'))

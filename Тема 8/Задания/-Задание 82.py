f3572f5caedcc29dc972b6a168036406c4daa7bd
Mihail <jrunetz@gmail.com>
2026-02-09 12:15:40 +0300

    Обновлен статус задания № 82 Тема: 8 > Неверно

---
+++ Тема 8/Задания/-Задание 82.py
@@ -0,0 +1,21 @@
+# Решение
+
+from itertools import product
+
+n = 0
+for p in product('АКОРСТ', repeat = 6):
+    n += 1
+    line = ''.join(p)
+    l = line[0]
+    if (l == 'Р' or l == 'О' or l == 'К') and line.count('О') == 2 and n % 2 == 0:
+        ans = line
+
+print(ans)
+
+answer = 'РТТООТ'
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 82, answer, '7ffb4e0ece07869880d51662a2234143'))

be67d3be104ade4a2804d68eb8bacf6c3b61f993
Андрей Боев <a.boev.it@gmail.com>
2026-02-16 14:05:49 +0300

    NEW: Тема 10

---
+++ Тема 10/Задания/Задание 108.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(10, 108, answer, 'c20ad4d76fe97759aa27a0c99bff6710'))
